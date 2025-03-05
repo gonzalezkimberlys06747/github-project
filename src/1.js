1b92c61529857bf5d5a19a95a3f388fe1f1d9cb2
gonzalezkimberlys06747 <gonzalez_kimberlys06747@gmx.com>
2025-03-05 04:18:49 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,6 @@
+// This is an example of a simple JavaScript program that generates a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log("The random number is: " + getRandomNumber());
